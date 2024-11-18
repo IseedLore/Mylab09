96aae2975e3d7c4c1e17067dc6e5740a8d02e9e3
Lorenzo Ferri - lorenzo.ferri21@studio.unibo.it <lorenzo.ferri21@studio.unibo.it>
2024-11-18 12:22:19 +0100

    Done SimpleGUI of 92-SIMPLE-MVC-IO

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,36 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    File file;
+    private static final String PATH = System.getProperty("user.home")
+                                        + System.getProperty("file.separator")
+                                        + "output.txt";
 
+    public void setFile(){
+        this.file = new File(PATH);
+    }
+    public File getFile(){
+        return file;
+    }
+    
+    public Path getPathFile(){
+        return file.toPath();
+    }
+
+    public void write(String s) throws IOException{
+        try(
+            PrintStream ps = new PrintStream(PATH, StandardCharsets.UTF_8)) 
+            {
+                    ps.print(s);
+            }
+    }
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,24 @@
 package it.unibo.mvc;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.security.PublicKey;
+import java.util.List;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +27,66 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 5;
+    Controller controller = new Controller();
+    
+    public SimpleGUI(){
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
 
+        /*Part 1 and 2 */
+        final JTextArea textArea = new JTextArea();
+        canvas.add(textArea, BorderLayout.CENTER);
+        final JButton save = new JButton("Save");
+        canvas.add(save, BorderLayout.SOUTH);
+        
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try{
+                controller.write(textArea.getText());
+                }catch(IOException e1){
+                    JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
+                    e1.printStackTrace(); // NOPMD: allowed as this is just an exercise
+                }
+            }
+            
+        });
+    }
+
+    private void display() {
+        /*
+         * Make the frame one fifth the resolution of the screen. This very method is
+         * enough for a single screen setup. In case of multiple monitors, the
+         * primary is selected. In order to deal coherently with multimonitor
+         * setups, other facilities exist (see the Java documentation about this
+         * issue). It is MUCH better than manually specify the size of a window
+         * in pixel: it takes into account the current resolution.
+         */
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this
+         * flag makes the OS window manager take care of the default positioning
+         * on screen. Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+        /*
+         * OK, ready to push the frame onscreen
+         */
+        frame.setVisible(true);
+    }
+    /**
+     * Launches the application.
+     *
+     * @param args ignored
+     */
+    public static void main(final String... args) {
+        new SimpleGUI().display();
+    }
 }
